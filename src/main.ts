40fbe7ee074bc0a0f2858f629a88c0bf0b7869c9
Zhenghao Li <lizhenghao99@outlook.com>
2021-10-02 21:30:40 -0700

    Configuration

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
+// noinspection JSIgnoredPromiseFromCall
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(8000);
+  app.useGlobalPipes(new ValidationPipe());
+
+  const port = process.env.PORT;
+  await app.listen(port);
+  const logger = new Logger();
+  logger.log(`Application listening on port ${port}...`);
 }
+
 bootstrap();
