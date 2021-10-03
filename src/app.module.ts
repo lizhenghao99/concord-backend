1470218bc32770b4af7d6df094c4dda0cb409e2e
Zhenghao Li <lizhenghao99@outlook.com>
2021-10-02 22:06:41 -0700

    Configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -25,11 +25,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
           type: 'postgres',
           autoLoadEntities: true,
           synchronize: true,
-          host: configService.get('DB_HOST'),
-          port: configService.get('DB_PORT'),
-          username: configService.get('DB_USERNAME'),
-          password: configService.get('DB_PASSWORD'),
-          database: configService.get('DB_DATABASE'),
+          url: configService.get('DATABASE_URL'),
         };
       },
     }),
--- src/config.schema.ts
+++ src/config.schema.ts
@@ -3,9 +3,5 @@ import * as Joi from 'joi';
 export const configSchema = Joi.object({
   PORT: Joi.number().default(8000),
   NODE_ENV: Joi.string().required(),
-  DB_HOST: Joi.string().required(),
-  DB_PORT: Joi.number().default(5432).required(),
-  DB_USERNAME: Joi.string().required(),
-  DB_PASSWORD: Joi.string().required(),
-  DB_DATABASE: Joi.string().required(),
+  DATABASE_URL: Joi.string().required(),
 });
